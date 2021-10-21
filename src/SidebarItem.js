9ebd011e202fd60f69e060fd798b5c99e8e1ba56
arpitashrivastav <81864506+arpitashrivastav@users.noreply.github.com>
2021-10-21 21:31:50 +0530

    Initial commit

---
+++ src/SidebarItem.js
@@ -0,0 +1,10 @@
+export default function SidebarItem(props) {
+  return (
+    <button
+      onClick={props.handleClick}
+      className={`sidebar-item ${props.active ? "active" : ""} `}
+    >
+      {props.name}
+    </button>
+  );
+}
